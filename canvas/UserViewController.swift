2cd032922302d24662dd19189e4680491b0795bc
samuraiotaku <bshimizu@csumb.edu>
2018-10-19 10:07:53 -0700

    at UseCase4

---
+++ canvas/UserViewController.swift
@@ -0,0 +1,75 @@
+//
+//  UserViewController.swift
+//  canvas
+//
+//  Created by Brandon Shimizu on 10/19/18.
+//  Copyright © 2018 Brandon Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat! = nil
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        var velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                       animations: { () -> Void in
+                        self.trayView.center = self.trayDown
+        }, completion: nil)
+        
+        if sender.state == .began {
+            print("Gesture began")
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            print("Gesture ended")
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayDown
+                }
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayUp
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        trayDownOffset = 250
+        trayUp = trayView.center // The initial position of the tray
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
